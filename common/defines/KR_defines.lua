143a6e1efe2602a73eecfd593f89de5bc2e65bb1
Alpinia <republic.alpinia@gmail.com>
2022-10-18 00:00:14 -0300

    0.22.2

--- common/defines/KR_defines.lua
+++ common/defines/KR_defines.lua
@@ -37,9 +37,6 @@ NDefines.NDiplomacy.VERY_GOOD_OPINION = 100									-- Vanilla is 50
 NDefines.NDiplomacy.VERY_BAD_OPINION = -100									-- Vanilla is -50
 NDefines.NDiplomacy.FRONT_IS_DANGEROUS = 0									-- Vanilla is -100
 
--- Trade
-NDefines.NTrade.BASE_TRADE_FACTOR = 100										-- Vanilla is 150
-
 -- World Tension
 NDefines.NCountry.CIVIL_WAR_INVOLVEMENT_MIN_TENSION = 0						-- Vanilla is 0.5
 NDefines.NDiplomacy.TENSION_STATE_VALUE = 0									-- Vanilla is 2
@@ -48,26 +45,24 @@ NDefines.NDiplomacy.TENSION_CB_WAR = 0										-- Vanilla is 5
 NDefines.NDiplomacy.TENSION_ANNEX_NO_CLAIM = 0								-- Vanilla is 2
 NDefines.NDiplomacy.TENSION_ANNEX_CLAIM = 0									-- Vanilla is 1
 NDefines.NDiplomacy.TENSION_ANNEX_CORE = 0									-- Vanilla is 0.5
-NDefines.NDiplomacy.TENSION_PUPPET = 0										-- Vanilla is 2
-NDefines.NDiplomacy.TENSION_GENERATE_WARGOAL = 0							-- Vanilla is 1
+NDefines.NDiplomacy.TENSION_PUPPET = 0										-- Vanilla is 1.25
+NDefines.NDiplomacy.TENSION_FORCE_GOVERNMENT = 0							-- Vanilla is 0.75
+NDefines.NDiplomacy.TENSION_GUARANTEE = 0									-- Vanilla is -5
+NDefines.NDiplomacy.TENSION_FACTION_JOIN = 0								-- Vanilla is 4
+NDefines.NDiplomacy.TENSION_JOIN_ATTACKER = 0								-- Vanilla is 2
+NDefines.NDiplomacy.TENSION_LIBERATE = 0									-- Vanilla is -1
+NDefines.NDiplomacy.TENSION_TAKE_ONE_CAPITAL_SHIP = 0						-- Vanilla is 0.25
+NDefines.NDiplomacy.TENSION_DEMILITARIZE_ZONE = 0							-- Vanilla is 0.25
+NDefines.NDiplomacy.TENSION_WAR_REPARATION = 0								-- Vanilla is 0.25
+NDefines.NDiplomacy.TENSION_RESOURCE_RIGHTS = 0								-- Vanilla is 0.25
+NDefines.NDiplomacy.TENSION_DISMANTLE_INDUSTRY = 0							-- Vanilla is 0.25
+NDefines.NDiplomacy.TENSION_CAPITULATE = 0									-- Vanilla is 0.4
 NDefines.NDiplomacy.TENSION_VOLUNTEER_FORCE_DIVISION = 0					-- Vanilla is 0.5
 NDefines.NDiplomacy.TENSION_SIZE_FACTOR = 0									-- Vanilla is 1.0
 NDefines.NDiplomacy.OPINION_FOR_DEMO_FROM_WT_GENERATION = 0					-- Vanilla is -2.0
 NDefines.NAI.MAX_THREAT_FOR_FIRST_YEAR_CIVILIAN_MODE = 30					-- Vanilla is 60
 
--- Peace Conference
-NDefines.NDiplomacy.BASE_PEACE_PUPPET_FACTOR = 0							-- Vanilla is 100
-NDefines.NDiplomacy.BASE_PEACE_LIBERATE_FACTOR = 0							-- Vanilla is 100
-NDefines.NDiplomacy.PEACE_SCORE_PER_PASS = 1.0								-- Vanilla is 0.2
-NDefines.NDiplomacy.PEACE_MIN_SCORE = 0.5									-- Vanilla is 0.3
-NDefines.NCountry.STATE_OCCUPATION_COST_MULTIPLIER = 0.10					-- Vanilla is 0.01
-
--- Navy
-NDefines.NNavy.SUPREMACY_PER_SHIP_BASE = 75											-- Vanilla is 100
-NDefines.NNavy.MAX_POSITIONING_PENALTY_FROM_HIGHER_SHIP_RATIO = 0.75				-- Vanilla is 0.5
-NDefines.NNavy.POSITIONING_PENALTY_FOR_SHIPS_JOINED_COMBAT_AFTER_IT_STARTS = 0.01	-- Vanilla is 0.05
-NDefines.NNavy.MAX_POSITIONING_PENALTY_FOR_NEWLY_JOINED_SHIPS = 0.025				-- Vanilla is 0.5
-NDefines.NNavy.POSITIONING_PENALTY_HOURLY_DECAY_FOR_NEWLY_JOINED_SHIPS = 0.05		-- Vanilla is 0.002
+NDefines.NDiplomacy.PEACE_SCORE_SCALE_FACTOR = 3.0							-- Vanilla is 1.35
 
 -- Volunteers
 NDefines.NAI.SEND_VOLUNTEER_EVAL_BASE_DISTANCE = 1000.0  					-- Vanilla is 175.0
@@ -81,7 +76,6 @@ NDefines.NGraphics.POLITICAL_GRID_SMALL_BOX_LIMIT = 12						-- Vanilla is 6
 NDefines.NGraphics.COUNTRY_FLAG_SMALL_TEX_WIDTH = 11						-- Vanilla is 10
 NDefines.NGraphics.COUNTRY_FLAG_STRIPE_TEX_MAX_WIDTH = 11					-- Vanilla is 10
 NDefines.NGraphics.VICTORY_POINT_MAP_ICON_TEXT_CUTOFF = {200, 350, 600}  	-- Vanilla is 100, 250, 500
-NDefines.NGraphics.VICTORY_POINTS_DISTANCE_CUTOFF = {300, 500, 1000} 		-- Vanilla is 300, 500, 1500
 
 -- General AI
 NDefines.NAI.RESEARCH_BONUS_FACTOR = 1.5									-- Vanilla is 0.9
@@ -123,12 +117,8 @@ NDefines.NAI.COMMUNISTS_ANTAGONIZE_COMMUNISTS = -10							-- Vanilla is -10
 -- Battleplan AI
 NDefines.NAI.HOUR_BAD_COMBAT_REEVALUATE = 42								-- Vanilla is 100
 NDefines.NAI.PLAN_ACTIVATION_SUPERIORITY_AGGRO = 1.2						-- Vanilla is 1.0
-NDefines.NAI.AI_FRONT_MOVEMENT_FACTOR_FOR_READY = 0.1						-- Vanilla is 0.25
-NDefines.NAI.MIN_PLAN_VALUE_TO_MICRO_INACTIVE = 0.1							-- Vanilla is 0.2
 
-NDefines.NAI.VP_LEVEL_IMPORTANCE_HIGH = 25									-- Not defined in vanilla
 NDefines.NAI.VP_LEVEL_IMPORTANCE_MEDIUM = 10								-- Vanilla is 10
-NDefines.NAI.VP_LEVEL_IMPORTANCE_LOW = 3									-- Not defined in vanilla
 
 -- Combat AI
 NDefines.NAI.MAX_DIST_PORT_RUSH = 40.0										-- Vanilla is 0.2
